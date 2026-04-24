1b598e3c1aaa9fa8c7ab4ba3c7165ac9f1bbe050
ddhanushd <dhanushdevaraj17@gmail.com>
2026-04-24 12:07:15 +0530

    ApacheKafka SpringBoot Producer demo

---
+++ src/main/java/com/kafka/kafkaproducerexample/service/KafkaMessagepublisher.java
@@ -0,0 +1,31 @@
+package com.kafka.kafkaproducerexample.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagepublisher {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaMessagepublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessageToTopic(String message){
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("KafkaTopic-demo", message);
+        future.whenComplete((result,ex)->{
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+
+    }
+}
