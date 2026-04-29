b622b12050dd6a94f7d491c918f84b96c29c6baa
ddhanushd <dhanushdevaraj17@gmail.com>
2026-04-29 12:30:08 +0530

    Kafka E2E Integration test with TestContainers- Producer

--- src/main/java/com/kafka/kafkaproducerexample/dto/Customer.java
+++ src/main/java/com/kafka/kafkaproducerexample/dto/Customer.java
@@ -1,11 +1,17 @@
 package com.kafka.kafkaproducerexample.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     private int id;
     private String name;
     private String email;
     private String contactNo;
+
+
 }
--- src/test/java/com/kafka/kafkaproducerexample/KafkaProducerExampleApplicationTests.java
+++ src/test/java/com/kafka/kafkaproducerexample/KafkaProducerExampleApplicationTests.java
@@ -1,13 +1,47 @@
 package com.kafka.kafkaproducerexample;
 
+import com.kafka.kafkaproducerexample.dto.Customer;
+import com.kafka.kafkaproducerexample.service.KafkaMessagepublisher;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class KafkaProducerExampleApplicationTests {
 
+    @Container
+    static KafkaContainer kafka =
+            new KafkaContainer(
+                    DockerImageName.parse("apache/kafka:3.8.0")
+            );
+
+    @DynamicPropertySource
+    public static void initKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaMessagepublisher publisher;
+
     @Test
-    void contextLoads() {
+    public void testSendEventsToTopic() {
+        publisher.sendEventsToTopic(new Customer(263, "test user", "test@gmail.com", "564782542752"));
+        await().pollInterval(Duration.ofSeconds(3))
+                .atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+                    // assert statement
+                });
     }
 
 }
